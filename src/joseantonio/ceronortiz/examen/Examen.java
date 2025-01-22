bd21850d10d47d26b117a115bbf1d57ae74e2c32
Jose-Antonio17 <ceronortizjose@gmail.com>
2025-01-22 19:32:50 +0100

    Examen

--- src/joseantonio/ceronortiz/examen/Examen.java
+++ src/joseantonio/ceronortiz/examen/Examen.java
@@ -5,26 +5,26 @@ public class Examen {
 	public static void main(String[] args) {
 
 		System.out.println("---------------------");
-		System.out.println("Creamos la lista de numeros:");
+		System.out.println("Creamos la lista de números:");
 		int[] numerosLista = UtilidadesExamen.creaArray();
 
 		System.out.println("---------------------");
-		System.out.println("Lista de numeros introducidos:");
+		System.out.println("Lista de números introducidos:");
 		UtilidadesExamen.mostrarArray(numerosLista);
 
 		System.out.println("---------------------");
-		System.out.println("Media de los numeros introducidos:");
+		System.out.println("Media de los números introducidos:");
 		float mediaNumeros = UtilidadesExamen.media(numerosLista);
 		System.out.println("La media es " + mediaNumeros);
 
 		System.out.println("---------------------");
-		System.out.println("Numero mayor y menor de los numeros introducidos:");
+		System.out.println("Número mayor y menor de los números introducidos:");
 		int[] extremos = UtilidadesExamen.encontrarExtremos(numerosLista);
 		System.out.println("El numero mayor es " + extremos[0]);
 		System.out.println("El numero menor es " + extremos[1]);
 
 		System.out.println("---------------------");
-		System.out.println("Numeros mayores de la media");
+		System.out.println("Números mayores de la media");
 		int[] numerosMayoresPromedio = UtilidadesExamen.filtrarMayoresQuePromedio(numerosLista, mediaNumeros);
 		for (int numero : numerosMayoresPromedio) {
 			System.out.println(numero);
